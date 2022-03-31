d7ee24f3118ef74c09d929c7e0b6d084f65b22c6
Stiven Gomez <stiven.gomez@nuvu.cc>
2022-03-30 22:19:50 -0500

    Add component cart.

--- lib/core/paths/app_paths.dart
+++ lib/core/paths/app_paths.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:shopbeer/gui/views/home_view/home_view.dart';
+import 'package:shopbeer/gui/templates/zoom_drawer.dart';
+import 'package:shopbeer/gui/views/cart_view/cart_view.dart';
 import 'package:shopbeer/gui/views/login_view/login_view.dart';
 
 Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
-  'home': ( _ ) => const HomeView(),
+  'home': ( _ ) => const ZoomDrawerApp(),
   'login': ( _ ) => const LoginView(),
+  'cart': ( _ ) => const CartView(),
 };
