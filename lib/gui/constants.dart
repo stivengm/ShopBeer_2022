df2e8b37b579f5f7b4fba3b72a6077e0af0d705d
Stiven Gomez <s4.gomez@praxisglobe.net>
2022-04-18 13:23:36 -0500

    Add color transparent.

--- lib/gui/constants.dart
+++ lib/gui/constants.dart
@@ -5,6 +5,7 @@ const Color backgroundApp = Color(0xFFebebeb);
 const Color whiteColor = Color(0xFFFFFFFF);
 const Color blackColor = Color(0xFF000000);
 const Color greyColor = Colors.grey;
+const Color transparent = Colors.transparent;
 
 const double defaultPadding = 16.0;
 const double defaultBorderRadius = 12.0;
