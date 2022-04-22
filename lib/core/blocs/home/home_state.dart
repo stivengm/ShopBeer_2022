0c5ee487f565a5fecccd5b7e4952e69f8f2bc9ee
Stiven Gomez <stiven.gomez@nuvu.cc>
2022-04-21 21:41:19 -0500

    Add aguardiente in homeBloc.

--- lib/core/blocs/home/home_state.dart
+++ lib/core/blocs/home/home_state.dart
@@ -5,27 +5,32 @@ class HomeState extends Equatable {
   final List<MethodsPayModel>? methodsPay;
   final List<ProductsModel>? productsModel;
   final List<ProductsModel>? productsCerveza;
+  final List<ProductsModel>? productsAguardiente;
   final bool isLoading;
 
   const HomeState({
     methodsPay,
     productsModel,
     productsCerveza,
+    productsAguardiente,
     this.isLoading = true
   }): methodsPay = methodsPay ?? const [],
       productsCerveza = productsCerveza ?? const [],
+      productsAguardiente = productsAguardiente ?? const [],
       productsModel = productsModel ?? const [];
 
   HomeState copyWith({
     List<MethodsPayModel>? methodsPay,
     List<ProductsModel>? productsModel,
     List<ProductsModel>? productsCerveza,
+    List<ProductsModel>? productsAguardiente,
     bool? isLoading,
   }) => HomeState(
     methodsPay: methodsPay ?? this.methodsPay,
     isLoading: isLoading ?? this.isLoading,
     productsModel: productsModel ?? this.productsModel,
-    productsCerveza: productsCerveza ?? this.productsCerveza
+    productsCerveza: productsCerveza ?? this.productsCerveza,
+    productsAguardiente: productsAguardiente ?? this.productsAguardiente,
   );
   
   // @override
