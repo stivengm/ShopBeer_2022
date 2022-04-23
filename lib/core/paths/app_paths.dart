58dce70f35f358e5c019996648ea4e835f9e1503
Stiven Gomez <s4.gomez@praxisglobe.net>
2022-04-22 23:09:09 -0500

    Add onBoarding component.

--- lib/core/paths/app_paths.dart
+++ lib/core/paths/app_paths.dart
@@ -5,6 +5,7 @@ import 'package:shopbeer/gui/views/details_product_view/details_product_view.dar
 import 'package:shopbeer/gui/views/gps_status_view/gps_status_view.dart';
 import 'package:shopbeer/gui/views/login_view/login_view.dart';
 import 'package:shopbeer/gui/views/notifications_view/notifications_view.dart';
+import 'package:shopbeer/gui/views/on_boarding_view/on_boarding_view.dart';
 import 'package:shopbeer/gui/views/register_view/register_view.dart';
 
 Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
@@ -12,6 +13,7 @@ Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
   'cart': ( _ ) => const CartView(),
   'login': ( _ ) => const LoginView(),
   'register': ( _ ) => const RegisterView(),
+  'onBoarding': ( _ ) => const OnBoardingView(),
   'gpsPermission': ( _ ) => const GpsStatusView(),
   'notifications': ( _ ) => const NotificationsView(),
   'detailsProduct': ( _ ) => const DetailProductView(),
---
+++ lib/gui/views/on_boarding_view/on_boarding_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingView extends StatefulWidget {
+  const OnBoardingView({ Key? key }) : super(key: key);
+
+  @override
+  State<OnBoardingView> createState() => _OnBoardingViewState();
+}
+
+class _OnBoardingViewState extends State<OnBoardingView> {
+  @override
+  Widget build(BuildContext context) {
+    int step = 1;
+    return Scaffold(
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return Column();
+  }
+}
