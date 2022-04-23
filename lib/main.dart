316e98e7a171acdc4a6c783e282d67a55a35e960
Stiven Gomez <s4.gomez@praxisglobe.net>
2022-04-22 20:16:09 -0500

    Add config fonts.

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
   MaterialApp mateApp() => MaterialApp(
       title: 'ShopBeer',
       theme: ThemeData(
+        fontFamily: 'Gordita',
         scaffoldBackgroundColor: backgroundApp,
         primarySwatch: Colors.deepPurple,
       ),
