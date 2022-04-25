8e69e17810316d29ba3f1036c0e58fda71ca1d3a
Stiven Gomez <s4.gomez@praxisglobe.net>
2022-04-24 19:03:33 -0500

    Add blocProvider login.

--- lib/core/providers/bloc_provider.dart
+++ lib/core/providers/bloc_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopbeer/core/blocs/gps/gps_bloc.dart';
 import 'package:shopbeer/core/blocs/home/home_bloc.dart';
+import 'package:shopbeer/core/blocs/login/login_bloc.dart';
 
 final List<BlocProvider> providerBloc = [
+  BlocProvider<LoginBloc>(create: (_) => LoginBloc()),
   BlocProvider<HomeBloc>(create: (_) => HomeBloc()),
   BlocProvider<GpsBloc>(create: (_) => GpsBloc()),
 ];
