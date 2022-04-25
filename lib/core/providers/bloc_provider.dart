b6afb01c5291a3156fff425c35124be990e90330
Stiven Gomez <s4.gomez@praxisglobe.net>
2022-04-24 20:55:49 -0500

    Add location bloc.

--- lib/core/providers/bloc_provider.dart
+++ lib/core/providers/bloc_provider.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopbeer/core/blocs/gps/gps_bloc.dart';
 import 'package:shopbeer/core/blocs/home/home_bloc.dart';
+import 'package:shopbeer/core/blocs/location/location_bloc.dart';
 import 'package:shopbeer/core/blocs/login/login_bloc.dart';
 
 final List<BlocProvider> providerBloc = [
   BlocProvider<LoginBloc>(create: (_) => LoginBloc()),
   BlocProvider<HomeBloc>(create: (_) => HomeBloc()),
   BlocProvider<GpsBloc>(create: (_) => GpsBloc()),
+  BlocProvider<LocationBloc>(create: (_) => LocationBloc()),
 ];
